8221f3492655c285f110a01df05ce0d69b8626c8
Martin Cortez <martco@gmail.com>
2019-12-29 13:18:07 -0800

    file naming logic

--- src/Canvas2Image.js
+++ src/Canvas2Image.js
@@ -1,7 +1,8 @@
 import { saveAs } from 'file-saver';
 
-export default ({canvas, filename, cb}) => {
+export default ({canvas, index, treatment, cb}) => {
   canvas.toBlob((blob) => {
+    const filename = index + "/" + treatment + ".jpeg";
     saveAs(blob, filename);
     cb(filename);
   });
